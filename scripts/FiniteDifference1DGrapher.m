8d0b99350b97c77e9733621943642385bb34bed1
ThomasGartman <thomasgartman@ku.edu>
2018-11-11 04:09:12 -0600

    Implemented Figure Generator and execution script.

--- scripts/FiniteDifference1DGrapher.m
+++ scripts/FiniteDifference1DGrapher.m
@@ -1,2 +1,23 @@
 %FiniteDifference1DGrapher in MATH647ComputerProject
+%Version 0.1.0 Last Edited November 11th, 2018
 %
+%Takes in a solution matrix generated from applying the Finite Difference 
+%Method and saves a surface plot + certain (u,x) plots
+%
+%Arguments:
+%
+%Preconditions:
+%
+%Postconditions:
+function[] = FiniteDifference1DGrapher(dataMatrix, xscale, tscale, tStep, figTitle, xtitle, ttitle, utitle, fileName)
+    surf(xscale, tscale, dataMatrix);
+    axis tight;
+    colormap(jet);
+    title(figTitle);
+    xlabel(xtitle);
+    ylabel(ttitle);
+    zlabel(utitle);
+    view(130,45);
+    saveas(gcf, fileName);
+    movefile(fileName, 'images');
+end
